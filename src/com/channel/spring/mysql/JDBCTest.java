596469fb6ba6da1092757ca88e4dabd86016d785
1212100124 <294141800@qq.com>
2017-09-02 14:24:51 +0800

    Can connect to mysql!

--- src/com/channel/spring/mysql/JDBCTest.java
+++ src/com/channel/spring/mysql/JDBCTest.java
@@ -2,6 +2,7 @@ package com.channel.spring.mysql;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
 
@@ -10,14 +11,22 @@ import javax.sql.DataSource;
  * Created by jackie on 17-9-1.
  */
 public class JDBCTest {
+    private static ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationMysql.xml");
+    private static  JdbcTemplate jdbcTemplate = ctx.getBean(JdbcTemplate.class);
     public static void main(String[] args) {
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationMysql.xml");
 
         try {
+
             DataSource dataSource = ctx.getBean(DataSource.class);
             System.out.println(dataSource.getConnection());
+            update();
         }catch (Exception e){
             e.printStackTrace();
         }
+    }
+
+    public static void update(){
+        String sql="UPDATE Name SET name = ? WHERE id = ?";
+        jdbcTemplate.update(sql,"channel",1);
     }
 }
